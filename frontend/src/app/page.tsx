e2758162d0f12604374dbb16d8ea93f988e86043
Tiago franzoni <86576812+TiagoFranzoni@users.noreply.github.com>
2023-11-22 10:39:58 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <h1> Welcome to the ConchaYOro App </h1>
+      <h1 style={{marginBottom: '20px'}}> Welcome to the ConchaYOro App </h1>
+      <h2 style={{color: 'blue'}}> ALTERAÇÃO FEITA! </h2>
     </div>
   </main>
   )
